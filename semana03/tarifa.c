f0906e2b41c8b59d45d709c850db269ce5de479e
Hiandro Alex <127314813+hiandro6@users.noreply.github.com>
2026-08-25 16:35:11 -0300

    Add consumption tariff calculation in tarifa.c (falta corrigir)
    Implement a program to calculate consumption tariffs based on user input.

---
+++ semana03/tarifa.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+	int consumo;
+	printf("digite o consumo: ");
+	scanf("%d", &consumo);
+
+	if (consumo < 0){
+		printf("consumo inválido");
+	
+	}
+
+	else {
+		if (consumo <= 100){
+			float tarifa = (consumo * 0.5);
+			printf("%d e o valor da sua tarifa", tarifa);
+	
+		}
+
+		else if ((consumo >= 101) && (consumo <= 200)){
+			printf("%d e o valor da sua tarifa", (consumo * 0.75));
+	
+		}
+
+		else if ((consumo > 200) && (consumo <= 500)){
+			printf("%d e o valor da sua tarifa", (consumo * 1.20));
+	
+		}
+
+		else if (consumo > 500){
+			printf("%d e o valor da sua tarifa", (consumo * 1.20) + (consumo * 0.15));
+	
+		}
+	}
+	
+	return 0;
+}
