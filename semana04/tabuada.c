148fff1e3cae5d6badd35eea0192bd88fd0b3ff0
Hiandro Alex <127314813+hiandro6@users.noreply.github.com>
2026-09-01 15:17:05 -0300

    Add multiplication table program in tabuada.c

---
+++ semana04/tabuada.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+
+
+        for (int i = 0; i <= 5; i++) {
+                for (int j = 0; j <= 10; j++) {
+                        printf("%d x %d = %d \n", i, j, i*j);
+                }
+
+        }
+
+        return 0;
+
+}
